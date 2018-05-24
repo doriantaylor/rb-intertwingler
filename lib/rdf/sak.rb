99d57209d89a7c876ab02111148f41827577a6e8
Dorian Taylor <code@doriantaylor.com>
2018-05-24 10:58:04 -0700

    big list of things i want this to do

--- lib/rdf/sak.rb
+++ lib/rdf/sak.rb
@@ -7,18 +7,70 @@ require 'commander'
 module RDF::SAK
 
   class Context
+    # holy cow this is actually a lot of stuff:
+
+    # turn markdown into xhtml (via md-noko)
+
+    # turn html into xhtml (trivial)
+
+    # generate triples from ordinary (x)html structure
+
+    # map vanilla (x)html metadata to existing graph (ie to get URIs)
+
+    # pull triples from rdfa
+
+    # stuff rdfa into rdfa-less xhtml
+
+    # basic nlp detection of terms + text-level markup (dfn, abbr...)
+
+    # markdown round-tripping (may as well store source in md if possible)
+
+    # add link titles
+
+    # segmentation of composite documents into multiple files
+
+    # aggregation of simple documents into composites
+
+    # generate backlinks
+
+    # generate indexes of people, groups, and organizations
+
+    # generate indexes of books, not-books, and other external links
+
+    # generate skos concept schemes
+
+    # generate atom feed
+
+    # generate sass palettes
+
+    # generate rewrite map
+
+    # read from source
+
+    # write (manipulated (x|x?ht)ml) back to source
+
+    # write to target
+
   end
 
   class CLI
+    # This is a command-line interface
+
     include XML::Mixup
     include Commander::Methods
 
+    # bunch of data declarations etc we don't want to expose
     private
 
+    # actual methods
     public
+
+    # constructor
 
     def initialize
     end
+
+    # vestigial
 
     def run
       run!
