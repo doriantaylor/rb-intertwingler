0205a4ff70837b27b2f002ad88d0145c49b48f53
Dorian Taylor <code@doriantaylor.com>
2023-09-15 10:07:58 -0400

    this might work?

--- experimental/parse-config.rb
+++ experimental/parse-config.rb
@@ -18,6 +18,53 @@ TFO  = Intertwingler::Vocab::TFO
 SH   = RDF::Vocab::SH
 XSD  = RDF::Vocab::XSD
 
+PATHS = {
+  SH.alternativePath => -> repo, o {
+    # this shoouuuuld be a list
+    list = RDF::List subject: o, graph: repo
+    list.reverse.reduce do |a, x|
+      SPARQL::Algebra::Operator::Alt.new x, a
+    end
+  },
+  SH.inversePath     => SPARQL::Algebra::Operator::Reverse,
+  SH.oneOrMorePath   => SPARQL::Algebra::Operator::PathPlus,
+  SH.zeroOrOnePath   => SPARQL::Algebra::Operator::PathOpt,
+  SH.zeroOrMorePath  => SPARQL::Algebra::Operator::PathStar,
+}
+
+# recursively trace the shacl predicate paths. the shacl module
+# appears to be really only about doing shacl shapes and appears to
+# turn the rdf into json-ld first before parsing it??? no idea.
+def algebra repo, subject, seen = {}
+  # it's either a sequence or one of these buggers
+  if !repo.objects_for(subject, RDF.first, only: :resource).empty?
+    list = RDF::List.new(subject: subject, graph: repo).map do |x|
+      algebra repo, x
+    end
+
+    return list.reverse.reduce do |a, x|
+      SPARQL::Algebra::Operator::Seq.new x, a
+    end
+  else
+    struct = repo.struct_for subject
+    # warn struct
+    # this should yield exactly one thing
+    keys = PATHS.keys & struct.keys
+    raise ArgumentError,
+      "more than one key: #{keys.sort.join ?,}" if keys.size > 1
+
+    if keys.empty?
+      # then this is a term
+      return subject
+    else
+      op   = PATHS[keys.first]
+      obj  = struct[keys.first].first # XXX this should only be one
+
+      return op.is_a?(Proc) ? op.call(repo, obj) : op.new(algebra repo, obj)
+    end
+  end
+end
+
 # load the graph
 repo = RDF::Repository.load ARGV.first
 
@@ -41,9 +88,44 @@ repo.objects_for(resolver, ITCV.prefix, only: :resource).each do |decl|
   prefix = repo.objects_for(decl, SH.prefix, only: :literal).sort.first
   ns     = repo.objects_for(
     decl, SH.namespace, only: :literal, datatype: XSD.anyURI).sort.first
-  pfx[prefix.to_s] = RDF::URI(ns.to_s)
+  # i don't understand why this but
+  pfx[prefix.to_s.to_sym] = RDF::URI(ns.to_s)
+end
+
+if v = repo.objects_for(resolver, ITCV.vocab).first
+  v = RDF::URI(v.to_s) if v.literal?
+  params[:vocab] = v
 end
 
 # 3. document class and fragment specifiers
 
-puts params.inspect
+params[:documents] = repo.objects_for(resolver, ITCV.document, only: :resource)
+
+params[:fragments] = {}
+
+repo.objects_for(resolver, ITCV.fragment, only: :resource).each do |frag|
+  specifier = {}
+
+  specifier[:fragment] = fc = repo.objects_for(frag,
+    ITCV[:"fragment-class"], only: :resource).sort.first
+
+  if host =  repo.objects_for(frag,
+    ITCV[:"host-class"], only: :resource).sort.first
+    specifier[:host] = host
+  end
+
+  repo.objects_for(frag, ITCV.via, only: :resource).each do |path|
+    # this will be either a term or a path object or a list
+    via = specifier[:via] ||= []
+    via << algebra(repo, path)
+  end
+
+  params[:fragments][fc] = specifier
+end
+
+# that it?
+
+require 'pry'
+binding.pry
+
+#puts "#{params}"
